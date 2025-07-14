857e288fad26bc12c9f35e27aec503b109da3fcc
Orkhan <guseynov.orkhan@gmail.com>
2025-07-14 16:30:47 +0200

    Simplify signature of get_subscriber.
    Used impl trait for getting rid of where clause.

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -6,19 +6,11 @@ use tracing_log::LogTracer;
 use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 
-pub fn get_subscriber<Sink>(
+pub fn get_subscriber(
     name: String,
     env_filter: String,
-    sink: Sink,
-) -> impl Subscriber + Send + Sync
-where
-    // This "weird" syntax is a higher-ranked trait bound (HRTB)
-    // It basically means that Sink implements the `MakeWriter`
-    // trait for all choices of the lifetime parameter `'a`
-    // Check out https://doc.rust-lang.org/nomicon/hrtb.html
-    // for more details.
-    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
-{
+    sink: impl for<'a> MakeWriter<'a> + Send + Sync + 'static,
+) -> impl Subscriber + Send + Sync {
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
     let formatting_layer = BunyanFormattingLayer::new(name, sink);
